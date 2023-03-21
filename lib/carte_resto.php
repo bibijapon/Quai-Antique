0f1950ac2daa713bf8a667aa48735c63fbdf1d7d
Barbara Sermondadaz <barbara.sermondadaz@laposte.net>
2023-03-21 19:03:00 +0100

    Ajout du dossier lib

---
+++ lib/carte_resto.php
@@ -0,0 +1,21 @@
+<?php
+    $plats = [
+        ['titre' => 'La Fondue Savoyarde', 'image' => 'fondue savoyarde.jpeg'],
+        ['titre' => 'La Raclette Savoyarde', 'image' => 'tradition raclette.jpeg'],
+        ['titre' => 'La Tartiflette Savoyarde', 'image' => 'Tartiflette savoyarde.jpeg'],
+    ];
+    $carte_plats = [
+        ['titre' => 'La Fondue Savoyarde', 'image' => 'carte fondue savoyarde.jpeg', 'prix' => '15 euros', 'description' => 'Fromage et vin issus de la ferme du coin'],
+        ['titre' => 'La Raclette Savoyarde', 'image' => 'carte tradition raclette.jpeg', 'prix' => '15 euros', 'description' => 'Fromage et vin issus de la ferme du coin'],
+        ['titre' => 'La Tartiflette Savoyarde', 'image' => 'carteTartiflette savoyarde.jpeg', 'prix' => '15 euros', 'description' => 'Fromage et vin issus de la ferme du coin'],
+    ];
+    $carte_entrees = [
+        ['titre' => 'Salade aux croustillants de reblochon', 'image' => 'Salade aux croustillants de reblochons.jpeg', 'prix' => '6 euros', 'description' => 'Agriculture Bio'],
+        ['titre' => 'Salade de crozets aux betteraves', 'image' => 'Salade de crozets aux betteraves.jpg', 'prix' => '6 euros', 'description' => 'Agriculture Bio'],
+        ['titre' => 'Soufflé au fromage savoyard', 'image' => 'Soufflé au fromage savoyard.jpeg', 'prix' => '4 euros', 'description' => 'Agriculture Bio'],
+    ];
+    $carte_desserts = [
+        ['titre' => 'Gateau de Savoie', 'image' => 'Gâteau de savoie.jpeg', 'prix' => '6 euros', 'description' => 'Agriculture Bio'],
+        ['titre' => 'Glace Dame Blanche', 'image' => 'glace dame blanche.jpg', 'prix' => '6 euros', 'description' => 'Agriculture Bio'],
+        ['titre' => 'Rissoles Savoyardes', 'image' => 'Rissoles savoyardes.jpeg', 'prix' => '4 euros', 'description' => 'Agriculture Bio'],
+    ];
---
+++ lib/config.php
@@ -0,0 +1,5 @@
+<?php
+    define('_IMG_PLATS_', 'img/plats/');
+    define('_IMG_CARTE_PLATS', 'img/carte/plats/');
+    define('_IMG_CARTE_ENTREES', 'img/carte/entree/');
+    define('_IMG_CARTE_DESSERTS', 'img/carte/dessert/');
